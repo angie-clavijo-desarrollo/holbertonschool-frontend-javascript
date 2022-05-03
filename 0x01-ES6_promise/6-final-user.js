4d73fa507cf669249186b94a37d6c651289d6662
AnguiClvijoGutierrez <angui.clavijo@imagineapps.co>
2022-05-02 20:37:58 -0500

    simple promise only reject

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
+
+  const fileData = await uploadPhoto(fileName).catch((err) => ({
+    status: 'rejected',
+    value: err.toString(),
+  }));
+
+  return Promise.resolve([userData, fileData]);
+}
