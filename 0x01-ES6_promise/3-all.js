38df90a083fef5de82a88307758a400028a95529
AnguiClvijoGutierrez <angui.clavijo@imagineapps.co>
2022-05-02 20:13:51 -0500

    promise all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
-/*promise all*/
-import { uploadPhoto, createUser } from './utils';
+/* eslint-disable */
+import { uploadPhoto, createUser } from "./utils.js";
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then((result) => {
+  return Promise.all([uploadPhoto(), createUser()]).then(result => {
     const resultBody = result[0].body;
     const resultfirstName = result[1].firstName;
     const resultlastName = result[1].lastName;
