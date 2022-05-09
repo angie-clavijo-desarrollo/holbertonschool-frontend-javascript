4948633d4f5e40ec204ba15c0dcc9f8fcbbec547
AnguiClvijoGutierrez <angui.clavijo@imagineapps.co>
2022-05-08 21:15:03 -0500

    exercise class

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+export default class Currency {
+  constructor(code, name) {
+    this._name = name;
+    this.code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+}
